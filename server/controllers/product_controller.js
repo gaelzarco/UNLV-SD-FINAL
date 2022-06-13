99470013fd2d825bdcc0d8e04c5e4c3537615976
gael <76548719+gaelzarco@users.noreply.github.com>
2022-06-13 10:46:54 -0700

    added products post route

--- server/controllers/product_controller.js
+++ server/controllers/product_controller.js
@@ -3,9 +3,26 @@ const db = require('../models')
 
 product.get('/', async (req, res) => {
     await db.Product.findAll()
-    .then(() => {
+    .then((products) => {
         return res.status(200).json(products)
     })
+})
+
+product.post('/', async (req, res) => {
+    const { name, ...rest } = req.body
+    if (!name || !rest) {
+        return res.status(400).json({ message: 'Please fill in all fields' })
+    }
+
+    if (!await db.Product.findOne({ name: name })) {
+        db.Product.create({
+            name,
+            ...rest
+        })
+        .then(() => {
+            return res.status(200).json({ message: 'Product added to catalog' })
+        })
+    }
 })
 
 module.exports = product
