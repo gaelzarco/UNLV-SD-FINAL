7f49edab36705ea4b0dd00e1586ba2d4ed9f3160
gael <76548719+gaelzarco@users.noreply.github.com>
2022-06-13 08:12:26 -0700

    create user working

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -4,13 +4,16 @@ const db = require('../models')
 
 user.post('/', (req, res) => {
     const { firstName, lastName, email, password, role } = req.body
+
     if(!firstName || !lastName || !email || !password || !role){
-        return res.status(400).json({msg: 'Please enter all fields'});
+        return res.status(400).json({message: 'Please enter all fields'});
     }
+    
     db.User.create(req.body)
     .then(() => {
         return res.status(200).json({ message: 'User created 200 OK' })
     }) 
+
     .catch(err => {
         console.log(`Error occured ${err}`)
         return res.status(400).json({ message: 'Something went wrong or user already exists' })
